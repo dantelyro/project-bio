0a4636beffd5710d86fc79bbb45542a8055a0d59
Dante <beyondmind9@gmail.com>
2025-02-09 11:35:38 -0300

    pricing and video

---
+++ app/components/landing-page/video-explanation.tsx
@@ -0,0 +1,9 @@
+import { PlayIcon } from "lucide-react";
+
+export function VideoExplanation () {
+  return (
+    <div className="border rounded-2xl border-border-primary aspect-video flex justify-center items-center my-20">
+      <PlayIcon className="text-accent-purple size-16" />
+    </div>
+  )
+}
