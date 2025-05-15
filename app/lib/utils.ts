c00ab15fc15fd219269ef8ad91d563c3bfcfef8d
dantelyro <beyondmind9@gmail.com>
2025-05-15 13:14:31 -0300

    feat: implement project creation with image upload and compression

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,8 +1,9 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import imageCompression from "browser-image-compression";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function sanitizeLink(link?: string) {
@@ -13,3 +14,33 @@ export function sanitizeLink(link?: string) {
     .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
     .toLocaleLowerCase();
 }
+
+export async function compressFiles(files: File[]) {
+  const compressPromisses = files.map(async (file) => {
+    try {
+      return await compressImage(file);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  });
+
+  return (await Promise.all(compressPromisses)).filter((file) => file !== null);
+}
+
+export const compressImage = (file: File): Promise<File> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      maxSizeMB: 0.2, // 200KB
+      maxWidthOrHeight: 900,
+      useWebWorker: true,
+      fileType: "image/png",
+    };
+
+    imageCompression(file, options)
+      .then((compressedFile) => {
+        resolve(compressedFile);
+      })
+      .catch((error) => reject(error));
+  });
+};
